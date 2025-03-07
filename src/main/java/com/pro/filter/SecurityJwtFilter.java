4a38ee587d22289e8f6393229194c9e7b6343261
yoe21c <yoe21c@gmail.com>
2025-03-07 12:21:10 +0900

    * 코멘트 드립니다.

--- src/main/java/com/pro/filter/SecurityJwtFilter.java
+++ src/main/java/com/pro/filter/SecurityJwtFilter.java
@@ -31,6 +31,8 @@ public class SecurityJwtFilter extends OncePerRequestFilter {
         try {
             jwtUtil.isExpired(accessToken);
         }catch (ExpiredJwtException e){
+
+            // todo 여기를 포함해서 다른곳에서도 예외로그나 혹은 실패로그를 남길때는 로그 레벨이 warn 이상으로 남기는 것이 좋습니다.
             log.info("access token expired");
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return;
@@ -38,6 +40,7 @@ public class SecurityJwtFilter extends OncePerRequestFilter {
 
         String category = jwtUtil.getCategory(accessToken);
 
+        // todo 코드레벨에 관한 이야기입니다. 여기 있는 로직이 jwtUtil 에 있는게 좋을것 같아요. 왜냐면 isExpired 라는 메소드가 이미 있어서 그것과 코드레벨을 맞추는게 좋을것 같습니다.
         if(!category.equals("access")){
             log.info("invaild access token");
         }
